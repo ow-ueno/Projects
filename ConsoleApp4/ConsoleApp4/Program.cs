10f5a9a6541442e27cad221f513829029edf1897
ow-ueno <66234056+ow-ueno@users.noreply.github.com>
2020-06-02 19:00:10 +0900

    1.2を土台だけ準備しました
    土台だけ

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+//200602は土台作っただけ
+//問題1.2だよ
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
