4327267d672afd888fd232b06afcf3ab52b7eda9
ow-ueno <66234056+ow-ueno@users.noreply.github.com>
2020-06-02 18:17:09 +0900

    演習1.1をやりました
    とても時間がかかった

---
+++ ConsoleApp3/ConsoleApp3/Product.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnotherNameSpace
+{
+    //商品クラス
+    public class Product
+    {
+
+        //商品コード
+        public int Code { get; private set; }
+        public string Name { get; private set; }
+        public int Price { get; private set; }
+
+        //税率が複雑なご時世なので下記追加
+        public bool IsReduced { get; private set; }
+        private const double ReducedTax = 0.08;
+        private const double Tax = 0.1;
+
+        //コンストラクタ
+        public Product(int code,string name,int price, bool isReduced){
+            this.Code = code;
+            this.Name = name;
+            this.Price = price;
+            this.IsReduced = isReduced;
+        }
+
+        //消費税額を求める
+        public int GetTax()
+        {
+
+            if (IsReduced)
+            {
+                return (int)(Price * ReducedTax);
+            }
+            else
+            {
+                return (int)(Price * Tax);
+            }
+
+        }
+
+        //税込価格を求める
+        public int GetPriceIncludingTax()
+        {
+
+            return (int)(Price + GetTax());
+
+        }
+
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+
+        //C#プログラミングのイディオム/定石&パターン
+        //p.42- 1章の演習問題1.1
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Hello World!");
+
+            //どら焼きオブジェクトの生成
+            //初期値(小問1)
+            AnotherNameSpace.Product BeanJamPancake = new AnotherNameSpace.Product(98, "どら焼き", 210, true);
+
+            //税額を出力する(小問2)
+            Console.WriteLine("{0} , {1}", BeanJamPancake.Name, BeanJamPancake.GetTax());
+
+            //きちんとコンソールを止める親切設計
+            Console.WriteLine("Press Any Key...");
+            Console.ReadKey();
+
+        }
+    }
+}
