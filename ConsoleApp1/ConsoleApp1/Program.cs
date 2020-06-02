e327b9628ff7f9da17c7665110668e8a1411b7a6
ow-ueno <66234056+ow-ueno@users.noreply.github.com>
2020-06-02 16:01:30 +0900

    Initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class App1
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoge World!");
+
+            MyPoint a = new MyPoint(10, 20, 30);
+            MyPoint b = a;
+
+            a.X = 80;
+            a.Z = 100;
+
+            Console.WriteLine("a = ( {0} , {1} )", a.X, a.Y);
+            Console.WriteLine("b = ( {0} , {1} )", b.X, b.Y);
+
+            Console.ReadKey();
+
+        }
+    }
+
+    class MyPoint
+    {
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public MyPoint(int x, int y, int z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //最初はいつもここから
+            Console.WriteLine("Hello World!");
+
+            if (args.Length >= 1 && args[0] == "-tom")
+            {
+                for (int feet = 1; feet <= 10; feet++)
+                {
+                    double meter = FeetToMeter(feet);
+                    Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+                }
+            }
+            else
+            {
+                for (int meter = 1; meter <= 10; meter++)
+                {
+                    double feet = MeterToFeet(meter);
+                    Console.WriteLine("{0} m = {1:0.0000} m", meter, feet);
+                }
+            }
+
+            Console.ReadKey();
+
+            Console.WriteLine("Press Any Key...");
+
+        }
+
+        //meter2feet
+        static double FeetToMeter(int feet)
+        {
+            return feet * 0.3048;
+        }
+
+        //meter2feet
+        static double MeterToFeet(int meter)
+        {
+            return meter * 3.2808;
+        }
+
+    }
+}
