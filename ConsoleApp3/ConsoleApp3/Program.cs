4f12be8c7dbb098ad6ace8b206d02e52d6c59649
ow-ueno <66234056+ow-ueno@users.noreply.github.com>
2020-06-02 18:35:56 +0900

    問題1.1の出力情報を追加しました。
    どら焼き以外のproductを追加し、それぞれに対して本体価格、軽減税率の対象可否、税額、税込価格を表示するようにしました。
    productsにaddする前にnew productを作っているのが微妙に非効率なような？

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Text;
+using System.Collections.Generic;
+using AnotherNameSpace;
 
 namespace ConsoleApp3
 {
@@ -15,13 +17,36 @@ namespace ConsoleApp3
             Console.WriteLine("Hello World!");
 
             //どら焼きオブジェクトの生成
+            //それ以外にもいろいろ追加
             //初期値(小問1)
-            AnotherNameSpace.Product BeanJamPancake = new AnotherNameSpace.Product(98, "どら焼き", 210, true);
+            List<Product> Products = new List<Product>();
+            Product BeanJamPancake = new Product(98, "どら焼き", 210, true);
+            Products.Add(BeanJamPancake);
+            Product Computer = new Product(99, "パソコン", 50000, false);
+            Products.Add(Computer);
+            Product Junos = new Product(100, "ゆず", 300, true);
+            Products.Add(Junos);
+            Product Monitor = new Product(101, "モニター", 12000, false);
+            Products.Add(Monitor);
 
             //税額を出力する(小問2)
-            Console.WriteLine("{0} , {1}", BeanJamPancake.Name, BeanJamPancake.GetTax());
 
-            //きちんとコンソールを止める親切設計
+            foreach (Product p in Products) {
+                Console.WriteLine("{0}の価格は{1}円です。", p.Name, p.Price);
+                Console.Write("{0}は軽減税率の対象", p.Name);
+                if (p.IsReduced) {
+                    Console.Write("な");
+                }
+                else
+                {
+                    Console.Write("ではない");
+                }
+                Console.Write("ので、");
+                Console.WriteLine("{0}の税額は{1}円です。", p.Name, p.GetTax());
+                Console.WriteLine("{0}の税込価格は{1}円です。", p.Name, p.GetPriceIncludingTax());
+            }
+
+            //最後にきちんとコンソールを止める親切設計
             Console.WriteLine("Press Any Key...");
             Console.ReadKey();
 
