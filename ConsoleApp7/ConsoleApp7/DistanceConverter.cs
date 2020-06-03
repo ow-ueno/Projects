cb35d08d7be0cfb50984864cbc7feebf960a8d95
ow-ueno <66234056+ow-ueno@users.noreply.github.com>
2020-06-03 12:13:25 +0900

    演習2.2ができました
    app2の書き換えなのでsoEasy

---
+++ ConsoleApp7/ConsoleApp7/DistanceConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp7
+{
+    class DistanceConverter
+    {
+
+        //constはprivateであるべき
+        private const double RATIOI2M = 0.0254;
+        private const double RATIOM2I = 39.3700;
+
+        //meter2feet
+        public static double InchToMeter(double feet)
+        {
+            return feet * RATIOI2M;
+        }
+
+        //meter2feet
+        public static double MeterToInch(double meter)
+        {
+            return meter * RATIOM2I;
+        }
+
+    }
+}
---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Exam3_2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //ここを変えれば出力する変換元がいくつからいくつまでかを制御できる
+            int start = 1;
+            int stop = 10;
+
+            if (args.Length >= 1 && args[0] == "-tom")
+            {
+                PrintMeterToInchList(start, stop);
+            }
+            else
+            {
+                PrintInchToMeterList(start, stop);
+            }
+
+            Console.WriteLine("Press Any Key...");
+
+            Console.ReadKey();
+        }
+
+        static void PrintInchToMeterList(int start, int stop)
+        {
+
+            for (int feet = start; feet <= stop; feet++)
+            {
+
+                double meter = DistanceConverter.InchToMeter(feet);
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, meter);
+
+            }
+
+        }
+
+        static void PrintMeterToInchList(int start, int stop)
+        {
+
+            for (int meter = start; meter <= stop; meter++)
+            {
+                double feet = DistanceConverter.MeterToInch(meter);
+                Console.WriteLine("{0} m = {1:0.0000} feet", meter, feet);
+            }
+
+        }
+
+    }
+}
