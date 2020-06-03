d0d0ef690d1f269c6865d2e85dd0f33ed57c3bfe
ow-ueno <66234056+ow-ueno@users.noreply.github.com>
2020-06-03 17:19:42 +0900

    カードゲームを追加しました
    デッキからカードが引けるようになりました

--- ConsoleApp10/ConsoleApp10/Program.cs
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ConsoleApp10
 {
-    class Program
+    //えぐざむ3.2だよ
+    //やりかけだよ
+    class Exam3_2
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello World!");
+
+            var names = new List<string>
+            {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
+
+            /* 3.2.1 Console.ReadLine
+             */
+            var line = Console.ReadLine();
+            names.FindIndex(n => n == line);
+
+
+            Console.WriteLine("Press Any Key...");
+            Console.ReadKey();
+
         }
     }
 }
